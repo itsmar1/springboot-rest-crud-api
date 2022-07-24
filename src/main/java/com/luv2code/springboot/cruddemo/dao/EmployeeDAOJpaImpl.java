c7ee0cfaa9e10060f89eaefcc3015980afd501d4
Marouane Ahal <d144013152@gmail.com>
2022-07-24 21:07:24 +0100

    testing the REST API with JPA DAO implementation

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -34,16 +34,33 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO{
 
     @Override
     public Employee findById(int theId) {
-        return null;
+
+        // get Employee
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+
+        // return employee
+        return theEmployee;
     }
 
     @Override
     public void save(Employee theEmployee) {
 
+        // save or update the employee
+        Employee dbEmployee = entityManager.merge(theEmployee);
+
+        // update with id from db ... so we can get generated id for save/insert
+        theEmployee.setId(dbEmployee.getId());
     }
 
     @Override
     public void deleteById(int theId) {
+
+        // delete object with primary key
+        Query theQuery = entityManager.createQuery("delete from Employee where id=:employeeId");
+
+        theQuery.setParameter("employeeId", theId);
+
+        theQuery.executeUpdate();
 
     }
 }
