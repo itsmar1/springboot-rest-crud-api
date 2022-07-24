4f884230b8fe473a73e6f8d7451cd9f5d92c7e6f
Marouane Ahal <d144013152@gmail.com>
2022-07-24 20:47:21 +0100

    creating JPA DAO implementation for REST API

---
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -0,0 +1,57 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
+@Repository
+public class EmployeeDAOJpaImpl implements EmployeeDAO{
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public EmployeeDAOJpaImpl(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+
+
+    @Override
+    public List<Employee> findAll() {
+
+        // create a query
+        Query theQuery = entityManager.createQuery("from Employee");
+
+        // execute query and get result list
+        List<Employee> employees = theQuery.getResultList();
+
+        // return the results
+        return employees;
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return null;
+    }
+
+    @Override
+    public void save(Employee theEmployee) {
+
+    }
+
+    @Override
+    public void deleteById(int theId) {
+
+    }
+}
+
+
+
+
+
+
+
+
